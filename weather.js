0a814c02cf2a19efdb678ac46b11b58d9996ecc2
Aaron Brelsford <aaronbrels@gmail.com>
2022-01-28 14:25:20 -0500

    In showPosition function I added lines to take my longitude and latitude convert to string, slice off some characters, and converted them back to a number.

--- weather.js
+++ weather.js
@@ -1,5 +1,16 @@
-let longitude = ""
-let latitude = ""
+
+// NEED TO FIX GEOLOCATOR. IN ORDER FOR WEATHER SERVICE API TO WORK MUST NOT BE MORE THAN FOUR DECIMAL POINTS
+
+
+// IN ORDER TO SLICE METHOD MUST FIRST CONVERT TO STRING. THOUGH IF I DO THAT WILL IT MESS UP THE FETCH
+
+// let longitudeNumber = ""
+// let latitudeNumber = ""
+
+// let longitudeString = longitudeNumber.toString()
+// console.log(longitudeString)
+
+
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition)
@@ -11,8 +22,34 @@ if (navigator.geolocation) {
 
 
 function showPosition(position) {
-     latitude = position.coords.latitude
-    longitude = position.coords.longitude
-    document.getElementById("latitude").innerHTML = latitude
-    document.getElementById("longitude").innerHTML = longitude
+     let longitudeNumber = position.coords.longitude
+     let latitudeNumber = position.coords.latitude
+     let longitudeString = longitudeNumber.toString()
+     let latitudeString = latitudeNumber.toString()
+    console.log(longitudeString)
+    let longSlice = longitudeString.slice(0,8)
+    console.log(longSlice)
+    let latSlice = latitudeString.slice(0,7)
+    console.log(latSlice)
+    let backToNumber = parseFloat(longSlice)
+    console.log(backToNumber)
+
+    document.getElementById("longitude").innerHTML = longitudeNumber
+    document.getElementById("latitude").innerHTML = latitudeNumber
+    
 }
+
+
+
+
+fetch("https://api.weather.gov/points/39.7216,-86.1538")
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    let forecast = "https://api.weather.gov/gridpoints/LWX/96,70/forecast"
+    fetch(forecast)
+    .then(response => response.json())
+    .then(forecastData => {
+        console.log(forecastData)
+    })
+})
